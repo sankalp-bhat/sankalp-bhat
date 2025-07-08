430401c9af52b26dd8fbb00a1515c5edd6d4ed77
sankalp-bhat <sankalp.b@research.iiit.ac.in>
2025-07-09 02:59:33 +0530

    Fixed deploy for vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
@@ -10,9 +9,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === "production" ? "/sankalp-bhat.github.io" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/sankalp-bhat.github.io/" : "",
+  }
 }
 
 module.exports = nextConfig
